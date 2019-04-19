b285ad4cfd792f6fe8c69e32b55a2f18204368e4
Anna <toanuta@gmail.com>
2019-04-19 23:59:01 +0300

    none

--- routing-xhr/magazine/js/Nav.js
+++
@@ -1,5 +0,0 @@
-const Nav = () => (
-  <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <Link className="navbar-brand" to="/routing-xhr/magazine/">Известное онлайн издание</Link>
-  </nav>
-)
